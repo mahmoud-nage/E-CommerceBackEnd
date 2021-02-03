29de45143d1e4dfe10e696dc4cd9b745fff6f64b
Mahmoud Nagy <54581364+mahmoud-nage@users.noreply.github.com>
2021-02-03 17:51:57 +0200

    3-2-2021

--- app/Models/Website/Notification.php
+++ app/Models/Website/Notification.php
@@ -8,15 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Notification extends Model
 {
-
     protected $table = 'notifications';
-
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
     protected $fillable = array('user_id', 'title_ar', 'title_en', 'body_ar', 'body_en', 'type', 'show_type', 'seen', 'country_id');
-    // protected $visible = array('user_id', 'title_ar', 'title_en', 'body_ar', 'body_en', 'type');
 
+    // protected $visible = array('user_id', 'title_ar', 'title_en', 'body_ar', 'body_en', 'type');
     public function users()
     {
         return $this->belongsToMany('App\Models\User', 'user_notifications');
@@ -31,5 +29,4 @@ class Notification extends Model
     {
         return $this->morphMany(Actions::class, 'actionable');
     }
-
 }
